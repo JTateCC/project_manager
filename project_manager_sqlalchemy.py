91eaa0b96d8d1a10b9e529bf8c9843bb0533e527
jtate <jtate.cc@outlook.com>
2021-12-12 21:54:00 +0000

    Defined SqlAlchemy Classes, added a file to hold database functions

--- project_manager_sqlalchemy.py
+++ project_manager_sqlalchemy.py
@@ -1,4 +1,29 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+
+# Define The Table Classes
+
+Base = declarative_base()
+
+
+class Project(Base):
+    __tablename__ = "Projects"
+    ProjectID = Column(Integer, primary_key=True)
+    Title = Column(String)
+    Description = Column(String)
+    Status = Column(Integer)
+    Deadline = Column(Integer)
+    Priority = Column(Integer)
+    Tasks = relationship("Task", backref=backref("Projects"))
+
+
+class Task(Base):
+    __tablename__ = "Tasks"
+    TaskID = Column(Integer, primary_key=True)
+    Description = Column(String)
+    Deadline = Column(Integer)
+    CompletionDate = Column(Integer)
+    Priority = Column(Integer)
+    ProjectID = Column(Integer, ForeignKey(Project.ProjectID))
 
